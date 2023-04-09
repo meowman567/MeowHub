432b1235786a19f25a33de6d01815711675bddb1
meowman567 <113253461+meowman567@users.noreply.github.com>
2023-04-09 14:40:37 -0400

    Create Whitelist.lua

---
+++ Whitelist.lua
@@ -0,0 +1,18 @@
+local Whitelist = {}
+
+Whitelist["Whitelisted"] = {
+    ["FB8C3969-823A-4FE6-94CE-DCBCD907D05F"] = {
+        ["Name"] = "Purg",
+        ["Rank"] = "Owner"
+    },
+    ["D511A5EA-ED26-48C1-B18C-A2D93BD1A702"] = {
+        ["Name"] = "Goofy Goober",
+        ["Rank"] = "Executive"
+    },
+    ["FB9DEC35-75C3-4408-A8F3-45B8737E3E60"] = {
+        ["Name"] = "Pft",
+        ["Rank"] = "Owner"
+    }
+}
+
+return Whitelist

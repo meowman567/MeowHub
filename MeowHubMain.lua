d5aedbd36ec46a4d88d9ac47935dab04c5af1e10
meowman567 <113253461+meowman567@users.noreply.github.com>
2023-04-09 16:22:45 -0400

    Create MeowHubMain.lua

---
+++ MeowHubMain.lua
@@ -0,0 +1,15 @@
+local Whitelist = loadstring(game:HttpGet('https://raw.githubusercontent.com/meowman567/MeowHub/main/Whitelist.lua'))()
+local CHWID = game:GetService("RbxAnalyticsService"):GetClientId()
+
+local Games = {
+    [4866692557] = "https://raw.githubusercontent.com/meowman567/MeowHub/main/GameScripts/AgeOfHeros.lua"
+}
+
+if Whitelist["Whitelisted"][CHWID] then
+    local PLID = game.PlaceId
+    if Games[PLID] then
+        loadstring(game:HttpGet(Games[PLID]))()
+    end
+else
+    print("You aint whitelisted gangy")
+end
